2e237ae29a7eac04e6c3e3f97a303f8575654aee
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-04-11 12:11:01 -0300

    Todos scripts num só arquivo

--- scripts-sql/create-database.sql
+++ scripts-sql/create-database.sql
@@ -1,126 +1,199 @@
-USE [master]
+CREATE DATABASE AulaCrud;
+GO
+                             
+USE AulaCrud
 GO
 
-/****** Object:  Database [Teste]    Script Date: 10/04/2021 15:34:56 ******/
-CREATE DATABASE [Teste]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Teste', FILENAME = N'C:\Users\stephan\Teste.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'Teste_log', FILENAME = N'C:\Users\stephan\Teste_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+/****** Object:  Table [dbo].[UF]    Script Date: 10/04/2021 15:36:00 ******/
+SET ANSI_NULLS ON
 GO
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [Teste].[dbo].[sp_fulltext_database] @action = 'enable'
-end
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET ANSI_NULL_DEFAULT OFF 
+CREATE TABLE [dbo].[UF](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](15) NOT NULL,
+	[Abbreviation] [varchar](2) NOT NULL,
+ CONSTRAINT [PK_UF] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-ALTER DATABASE [Teste] SET ANSI_NULLS OFF 
+/****** Object:  Table [dbo].[Cidade]    Script Date: 10/04/2021 15:35:52 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET ANSI_PADDING OFF 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET ANSI_WARNINGS OFF 
+CREATE TABLE [dbo].[Cidade](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](26) NOT NULL,
+	[IdUf] [int] NOT NULL,
+	[Capital] [bit] NOT NULL,
+ CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-ALTER DATABASE [Teste] SET ARITHABORT OFF 
+ALTER TABLE [dbo].[Cidade]  WITH CHECK ADD  CONSTRAINT [FK_City_UF] FOREIGN KEY([IdUf])
+REFERENCES [dbo].[UF] ([Id])
 GO
 
-ALTER DATABASE [Teste] SET AUTO_CLOSE OFF 
+ALTER TABLE [dbo].[Cidade] CHECK CONSTRAINT [FK_City_UF]
 GO
 
-ALTER DATABASE [Teste] SET AUTO_SHRINK OFF 
+/****** Object:  Table [dbo].[Usuario]    Script Date: 10/04/2021 15:36:14 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET AUTO_UPDATE_STATISTICS ON 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET CURSOR_CLOSE_ON_COMMIT OFF 
+CREATE TABLE [dbo].[Usuario](
+	[CPF] [varchar](14) NOT NULL,
+	[Name] [nvarchar](26) NOT NULL,
+	[IdCity] [int] NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[CPF] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-ALTER DATABASE [Teste] SET CURSOR_DEFAULT  GLOBAL 
+/****** Object:  StoredProcedure [dbo].[UpdateUser]    Script Date: 10/04/2021 15:38:06 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET CONCAT_NULL_YIELDS_NULL OFF 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET NUMERIC_ROUNDABORT OFF 
+CREATE	Procedure [dbo].[UpdateUser]
+@Name varchar(26),
+@Cpf varchar(14),
+@NewCpf varchar(14),
+@IdCity int
+AS
+UPDATE Usuario
+SET Name =@Name,
+    CPF = @NewCpf,
+    IdCity = @IdCity
+WHERE Cpf=@Cpf
 GO
 
-ALTER DATABASE [Teste] SET QUOTED_IDENTIFIER OFF 
+/****** Object:  StoredProcedure [dbo].[SelectUser]    Script Date: 10/04/2021 15:37:54 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET RECURSIVE_TRIGGERS OFF 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET  DISABLE_BROKER 
+CREATE Procedure [dbo].[SelectUser]
+@Cpf varchar(14)
+AS
+SELECT 
+    u.Name,
+    u.Cpf,
+    c.Name as City,
+    c.Id as IdCity,
+    c.IdUf
+FROM Usuario u JOIN Cidade c
+ON u.IdCity = c.Id
+WHERE cpf = ISNULL(@Cpf,cpf)
 GO
 
-ALTER DATABASE [Teste] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+/****** Object:  StoredProcedure [dbo].[SelectUf]    Script Date: 10/04/2021 15:37:41 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET DATE_CORRELATION_OPTIMIZATION OFF 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET TRUSTWORTHY OFF 
+CREATE Procedure [dbo].[SelectUf]
+
+AS
+SELECT Id, Name, Abbreviation FROM Uf ORDER BY Name
 GO
 
-ALTER DATABASE [Teste] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+
+/****** Object:  StoredProcedure [dbo].[SelectCity]    Script Date: 10/04/2021 15:37:28 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET PARAMETERIZATION SIMPLE 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET READ_COMMITTED_SNAPSHOT OFF 
+create Procedure [dbo].[SelectCity]
+@IdUf int
+AS
+SELECT Id, Name, IdUf, Capital FROM Cidade WHERE IdUF = @IdUf ORDER BY [Name]
 GO
 
-ALTER DATABASE [Teste] SET HONOR_BROKER_PRIORITY OFF 
+/****** Object:  StoredProcedure [dbo].[InsertUser]    Script Date: 10/04/2021 15:37:18 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET RECOVERY SIMPLE 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET  MULTI_USER 
+CREATE	Procedure [dbo].[InsertUser]  --INSERT PROC NAME HERE
+@Name varchar(26),
+@Cpf varchar(14),
+@IdCity int
+AS
+INSERT INTO Usuario ([Name], Cpf, IdCity) VALUES (@Name, @Cpf, @IdCity)
 GO
 
-ALTER DATABASE [Teste] SET PAGE_VERIFY CHECKSUM  
+
+/****** Object:  StoredProcedure [dbo].[InsertUf]    Script Date: 10/04/2021 15:37:06 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET DB_CHAINING OFF 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+ CREATE	Procedure [dbo].[InsertUf]  --INSERT PROC NAME HERE
+@Name varchar(26),
+@Abbreviation varchar(2)
+
+AS
+INSERT INTO Uf (Name, Abbreviation)
+OUTPUT Inserted.Id
+VALUES (@Name, @Abbreviation)
 GO
 
-ALTER DATABASE [Teste] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+
+/****** Object:  StoredProcedure [dbo].[InsertCity]    Script Date: 10/04/2021 15:36:56 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE [Teste] SET DELAYED_DURABILITY = DISABLED 
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE [Teste] SET QUERY_STORE = OFF
+ CREATE	Procedure [dbo].[InsertCity]  --INSERT PROC NAME HERE
+@Name varchar(26),
+@Capital bit,
+@IdUf int
+AS
+INSERT INTO Cidade (Name, IdUf, Capital)
+OUTPUT Inserted.Id
+VALUES (@Name, @IdUf, @Capital)
 GO
 
-USE [Teste]
+/****** Object:  StoredProcedure [dbo].[DeleteUser]    Script Date: 10/04/2021 15:36:47 ******/
+SET ANSI_NULLS ON
 GO
 
-ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+CREATE Procedure [dbo].[DeleteUser]
+@Cpf varchar(14)
+AS
+DELETE FROM Usuario WHERE Cpf = @Cpf
 GO
-
-ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
-GO
-
-ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
-GO
-
-ALTER DATABASE [Teste] SET  READ_WRITE 
-GO
-
