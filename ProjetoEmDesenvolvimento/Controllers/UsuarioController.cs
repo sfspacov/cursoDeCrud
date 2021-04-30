dc90f788a9b4e8b5417905d50b578f06b24b2728
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-04-29 22:22:53 -0300

    aula 17

--- ProjetoEmDesenvolvimento/Controllers/UsuarioController.cs
+++ ProjetoEmDesenvolvimento/Controllers/UsuarioController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SiteWeb.Models;
+using System;
 
 namespace MeuCrud.Controllers
 {
     public class UsuarioController : Controller
     {
-        public void CriarNovoUsuario(Usuario usuario)
-        {           
-            usuario.Salvar(usuario);
+        [HttpPost]
+        public bool CriarNovoUsuario([FromBody]Usuario user)
+        {
+            if (user.CPF == "" || user.Nome == "" || user.IdCity == 0)
+            {
+                throw new Exception("Preencha todos os campos");
+            }
+            else
+            {
+                user.Salvar(user);
+                return true;
+            }
+            
 
             //NÃO TEM LÓGICA DE GRAVAR NO BANCO DE DADOS NA CONTROLLER
         }
--- ProjetoEmDesenvolvimento/wwwroot/js/site.js
+++ ProjetoEmDesenvolvimento/wwwroot/js/site.js
@@ -1,25 +1,46 @@
 ﻿async function Salvar() {
-    var e = document.getElementById("comboCidade");
-    var idCidade = e.value;
-
+    let e = document.getElementById("comboCidade");
+    let idCidade = e.value;
+    let method = "POST";
     let user =
     {
         nome: document.getElementById("txtNome").value,
         cpf: document.getElementById("txtCpf").value,
         idCity: idCidade
     }
-    let url =
-        "/usuario/CriarNovoUsuario" +
-        "?nome=" + user.nome +
-        "&cpf=" + user.cpf +
-        "&idCity=" + user.idCity
-   
-    await fetch(url, { method: "GET" });
+
+    if (user.cpf == "" || user.nome == "" || user.idCity == 0) {
+        alert("Preencha todo os campos");
+    }
+    else {
+        let url = "/usuario/CriarNovoUsuario";
+
+        await fetch(url,
+            {
+                method: "POST",
+                body: JSON.stringify(user),
+                headers:
+                {
+                    'Content-Type': 'application/json;charset=utf-8'
+                }
+            })
+            .then(response => {
+                debugger;
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then(x => {
+                debugger;
+                alert('ok = ' + x.ok)
+            })
+    }
 }
 /*
  * CREATE   =   POST
  * READ     =   GET
  * UPDATE   =   PUT
  * DELETE   =   DELETE
- * 
+ *
  * */
