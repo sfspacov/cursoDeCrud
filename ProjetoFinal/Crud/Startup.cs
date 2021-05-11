e4a453f2f6c3b2e6225b7f9834b931f1efc635c7
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-05-11 17:37:35 -0300

    Health check nuget

--- ProjetoFinal/Crud/Startup.cs
+++ ProjetoFinal/Crud/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Crud
 {
-    public class Startup
+    public class Startup : BaseEntity
     {
         public Startup(IConfiguration configuration)
         {
@@ -27,11 +27,7 @@ namespace Crud
             services.AddScoped<IUsuario, Usuario>();
             services.AddHealthChecks()
             // Add a health check for a SQL Server database
-            .AddCheck(
-                "AulaCrud-check",
-                new DbHeatlCheck(),
-                HealthStatus.Unhealthy,
-                new string[] { "AulaCrudDb" });
+            .AddSqlServer(connectionString);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
