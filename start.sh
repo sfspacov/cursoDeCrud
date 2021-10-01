da78a85fe908c3a8cbbd142963b3793573811268
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-10-01 01:01:35 -0300

    shell script de start com docker image de sql personalizada

--- start.sh
+++ start.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-
-##########################################################################
-# Set output color variables
-
 BLUE='\033[1;34m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 
-printf "\n{$GREEN}Starting..."
+printf "${GREEN}STARTED\n\n"
 
 {
-	printf "\n{$GREEN}docker run database"
-	docker run spakov/banco_aulas:blackWomen -d
+    containersRunning=$(docker ps | grep sql-server-crud | wc -l)
 } && {
-	printf "\n{$GREEN}dotnet run project"
-	dotnet run ./src/Crud.sln --force --configuration Debug
+    if [ "${containersRunning}" -eq 0 ]; then
+		printf "${YELLOW} - docker run${NC}\n\n"
+		docker run --name sql-server-crud -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -e "MSSQL_PID=Express" -p 1433:1433 -d spakov/banco_aulas:blackWomen
+    fi
+}  && { 	
+	{
+		printf "${YELLOW} - dotnet build${NC}\n\n"
+		dotnet build ./src/Crud.sln --force --configuration Debug
+	} && {
+		printf "${YELLOW} - dotnet run${NC}\n\n"
+		dotnet ./src/Crud/bin/Debug/net5.0/Crud.dll
+	}
+} || {
+	printf "${RED}\n\nSTOPED!\n\n"
 }
